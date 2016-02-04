a1c1ccbd201710c5689382093086cb26b84d2b29
Julia Redston <jredston3@gatech.edu>
2016-02-04 17:35:53 -0500

    Began creating our login page, added text boxes and button

--- app/src/main/java/com/android/recsfromtechs/MainActivity.java
+++ app/src/main/java/com/android/recsfromtechs/MainActivity.java
@@ -1,17 +1,35 @@
 package com.android.recsfromtechs;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private HashMap<String,String> usernames;
+
+
+
+    //onCreate, as the name implies, this method is called when the app is "created" or opened
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //initializing our usernames map
+        usernames = new HashMap<String,String>();
+        //hardcoding a username and password for now because we don't have a registration page
+        usernames.put("bob","password");
+
     }
 
     @Override
@@ -34,5 +52,54 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    //Called when the login button is pressed
+    //essentially acts as an onclick listener, but this is done through xml and easier to write
+    //if you go into activity_main.xml and look under the button
+    // you'll see android:onClick="login"
+    //that means whenever the button is clicked it will call "login" which I've defined here
+    public void login(View view){
+        //first we need to find the text that was entered
+        // findViewById(R.id.idnamehere) is something we're going to use a lot
+        //it connects the objects in our xml file to objects in our java file
+        EditText usernameText = (EditText) findViewById(R.id.username);
+        EditText passwordText = (EditText) findViewById(R.id.password);
+
+        String storedPassword = usernames.get(usernameText.getText().toString());
+
+        if(storedPassword != null && storedPassword.equals(passwordText.getText().toString())){
+            //the username exists in our usernames map and the password matches the password stored
+            //storedPassword will be null if the username entered does not exist in our map
+            //user should be logged in and taken to another page
+
+
+
+
+            //Toasts! a funny name, theyre just little popups
+            //Since we dont have a second page right now, we can use this to see if our login is working
+            Context context = getApplicationContext();
+            CharSequence text = "Login successful";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        } else {
+            //Login was incorrect,
+            Context context = getApplicationContext();
+            CharSequence text = "username or password incorrect";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+
+
+
+
+
+
+
     }
 }
